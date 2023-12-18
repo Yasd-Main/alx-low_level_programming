4e2e9a810e7282e0969401d4538aa173f62234fb
Yasd-Main <kingyasd0192@gmail.com>
2023-12-18 07:47:07 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - A program that print a line using the printf function
  * Return: 0 (Success)
  */
 int main(void)
 {
-printf("ith proper grammar, but the outcome is a piece of art,\n")
+printf("With proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
